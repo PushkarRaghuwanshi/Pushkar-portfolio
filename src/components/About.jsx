57fcb103586efdcdef9be12982c7005ed9a63c08
Pushkar Raghuwanshi <pushkarraghuwanshi34@gmail.com>
2024-09-29 18:38:51 +0530

    fix: some build errors

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import Tilt from "react-tilt";
+// About.jsx
+import Tilt from 'react-parallax-tilt';
+
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
